2d51bbc8bf47b56f2b14e6479b153361d4f56b73
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-23 19:01:02 +0300

    Completed Day 1

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+	var allWords = [String]()
+	var usedWords = [String]()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+
+		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"),
+		   let startWords = try? String(contentsOf: startWordsURL) {
+			allWords = startWords.components(separatedBy: "\n")
+		}
+
+		if allWords.isEmpty {
+			allWords = ["silkworm"]
+		}
+
+		startGame()
 	}
 
+	private func startGame() {
+		title = allWords.randomElement()
+		usedWords.removeAll(keepingCapacity: true)
+		tableView.reloadData()
+	}
 
+	@objc private func promptForAnswer() {
+		let alertController = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+		alertController.addTextField()
+
+		let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] _ in
+			guard let answer = alertController?.textFields?[0].text else { return }
+			self?.submit(answer)
+		}
+
+		alertController.addAction(submitAction)
+		present(alertController, animated: true)
+	}
+
+	private func submit(_ answer: String) {
+		// TODO: Submit process
+	}
 }
 
+extension ViewController {
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		usedWords.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+		cell.textLabel?.text = usedWords[indexPath.row]
+		return cell
+	}
+}
