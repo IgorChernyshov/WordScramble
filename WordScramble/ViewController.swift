5b1e29a5f71a437a982e685130ed2bfee2b7d6ba
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-06-24 10:39:36 +0300

    Completed Day 2

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -41,15 +41,69 @@ class ViewController: UITableViewController {
 
 		let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] _ in
 			guard let answer = alertController?.textFields?[0].text else { return }
-			self?.submit(answer)
+			self?.submit(answer: answer)
 		}
 
 		alertController.addAction(submitAction)
 		present(alertController, animated: true)
 	}
 
-	private func submit(_ answer: String) {
-		// TODO: Submit process
+	private func submit(answer: String) {
+		let lowercasedAnswer = answer.lowercased()
+
+		let errorTitle: String
+		let errorMessage: String
+
+		if isPossible(word: lowercasedAnswer) {
+			if isOriginal(word: lowercasedAnswer) {
+				if isReal(word: lowercasedAnswer) {
+					usedWords.insert(answer, at: 0)
+					let indexPath = IndexPath(row: 0, section: 0)
+					tableView.insertRows(at: [indexPath], with: .automatic)
+					return
+				} else {
+					errorTitle = "Word not recognised"
+					errorMessage = "You can't just make them up, you know!"
+				}
+			} else {
+				errorTitle = "Word used already"
+				errorMessage = "Be more original!"
+			}
+		} else {
+			guard let title = title?.lowercased() else { return }
+			errorTitle = "Word not possible"
+			errorMessage = "You can't spell that word from \(title)"
+		}
+
+		let alertController = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default))
+		present(alertController, animated: true)
+	}
+
+	private func isPossible(word: String) -> Bool {
+		guard var tempWord = title?.lowercased() else { return false }
+
+		for letter in word {
+			if let position = tempWord.firstIndex(of: letter) {
+				tempWord.remove(at: position)
+			} else {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	private func isOriginal(word: String) -> Bool {
+		!usedWords.contains(word)
+	}
+
+	private func isReal(word: String) -> Bool {
+		let checker = UITextChecker()
+		let range = NSRange(location: 0, length: word.utf16.count)
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+		return misspelledRange.location == NSNotFound
 	}
 }
 
